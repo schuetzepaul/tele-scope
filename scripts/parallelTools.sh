4653e5713d3a5d832b73fd0747203cb028c46890
chrisjcc <christian.contreras@desy.de>
2016-04-13 14:13:09 +0200

    add naf submission scripts

---
+++ scripts/parallelTools.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+if [ `hostname | grep "nafhh"` ]; then
+    echo "Running on the NAF - so let's submit our jobs, job output will be stored in batch_output/..."
+    mkdir -p "batch_output"
+    
+    #define wait function - on the NAF, we just qsub. So let's wait until jobs have been submitted
+    w() {
+        wait
+    }
+    
+    isNAF=1
+    
+    if grep -q "slc6" <<< "$SCRAM_ARCH"; then
+        echo "Running at SL6 scram architecture. Submitting jobs to SL6 nodes."
+        TA="qsub -l distro=sld6 -@ $SCRIPTPATH/qsubParams.txt $SCRIPTPATH/../tele"
+	SA="qsub -l distro=sld6 -@ $SCRIPTPATH/qsubParams.txt $SCRIPTPATH/../scope"
+    else
+        TA="qsub -@ $SCRIPTPATH/qsubParams.txt $SCRIPTPATH/../tele"
+	SA="qsub -@ $SCRIPTPATH/qsubParams.txt $SCRIPTPATH/../scope"
+    fi
+else
+    w() {
+        while [ `ps ax | grep -E 'tele|scope' | wc -l` -gt 10 ]; do
+            sleep 1;
+        done
+    }
+
+    isNAF=0
+    TA=tele
+    SA=scope
+fi
---
+++ scripts/teleScript.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+# Provide user with the given parameters
+echo "Options provided: $@"
+
+USAGE="Usage: `basename $0` [-hv] [-o arg] [-f arg] [-r arg] args"
+
+# Parse command line options.
+while getopts hvo:f:r: OPT; do
+    case "$OPT" in
+        h)
+            echo $USAGE
+            exit 0
+            ;;
+        v)
+            echo "`basename $0` version 0.1"
+            exit 0
+            ;;
+        o)
+            OUTPUT_FILE=$OPTARG
+            ;;
+        f)
+            INPUT_FILE=$OPTARG
+            ;;
+        r)
+	    STRING=`echo $OPTARG | awk -F "," '{for(i=1; i <= NF; i++) printf "%s ",$i}'`
+	    array=(${STRING})
+
+	    for elem in "${array[@]}"
+	    do
+		if [[ $elem =~ "-" ]] ;then
+		    element=`echo $n | awk -F "-" '{for(i=$1; i <= $2; i++) printf "%s ",i}'`
+		    RUNS+=($element)  
+		else
+		    RUNS+=($elem)
+		fi
+	    done
+            ;;
+        \?)
+            # getopts issues an error message
+            echo $USAGE >&2
+            exit 1
+            ;;
+    esac
+done
+
+# We want at least one non-option argument. 
+# Remove this block if you don't need it.
+if [ $# -eq 0 ]; then
+    echo $USAGE >&2
+    exit 1
+fi
+
+# Set environmental variables to corresponding script path
+source $(dirname `readlink -f $0`)/parallelTools.sh
+
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+GEO=""
+RUN=""
+
+# Setup regex expressions
+regex_run="run ([0-9]*)"
+regex_geo="geo (geo_.*.dat)"
+
+# Loop through input file to set a map with key-value pair (ie. run, geo.dat file)
+while read LINE
+do
+    if [[ $LINE =~ "#" ]];then
+        continue
+    fi
+
+    if [[ $LINE =~ $regex_run ]]; then RUN=${BASH_REMATCH[1]}; fi
+    if [[ $LINE =~ $regex_geo ]]; then GEO=${BASH_REMATCH[1]}; fi
+
+    if [[ $RUN != "" ]] && [[ $GEO != "" ]];then
+	if [[ "${RUNS[@]}" =~ "${RUN}" ]] || [[ ${#RUNS[@]} == 0 ]]; then	    
+	    GEOFILES[$RUN]=$GEO
+	else
+	    continue
+	fi
+    fi
+done < $INPUT_FILE
+
+
+for RUN in "${!GEOFILES[@]}";do
+    $TA -g ${GEOFILES[$RUN]} $RUN &
+done
+
+wait
+
+if [ "$isNAF" = 1 ]; then
+    echo "Please check your jobs with qstat -u $USER | grep tele"
+else
+    echo "Processing all nominal samples finished!"
+fi
+
